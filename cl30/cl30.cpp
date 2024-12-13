c38ff64360c5a3d9288d2681ad905372ef5d9881
developeeer-level1 <yak.yakovochka@gmail.com>
2024-12-13 19:19:37 +0200

    a

--- cl30/cl30.cpp
+++ cl30/cl30.cpp
@@ -1,170 +1,87 @@
 #include <iostream>
-
 using namespace std;
 
-class Gadget {
-protected:
-    string brand;
-    Gadget(string a) {
-        brand = a;
-    }
-};
-
-class Mobile :protected Gadget {
-protected:
-    string batterySize;
+class Computer {
 public:
-    Mobile(string a, string b) : Gadget(a) {
-        batterySize = b;
+    void processorIsWork() {
+        cout << "Processor Work" << endl;
+    }
+    void graphicsCardIsWork() {
+        cout << "Graphics Card Work" << endl;
+    }
+    void motherboardIsWork() {
+        cout << "Motherboard Work" << endl;
+    }
+    void ramIsWork() {
+        cout << "RAM Work" << endl;
+    }
+    void hardDriveIsWork() {
+        cout << "Hard Drive Work" << endl;
     }
 };
 
-class Stationary : protected Gadget {
-protected:
-    string diagonal;
-public:
-    Stationary(string m, string t) : Gadget(m) {
-        diagonal = t;
-    }
-    
-};
-
-class Tablet : protected Mobile {
+class Processor {
 private:
-    string screenSize;
+    Computer engine;
 public:
-    Tablet(string a, string b, string c) : Mobile(a,b) {
-        screenSize = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand <<"\nBattery Size: "<< batterySize <<"\nScreen Size: "<< screenSize;
+    void printer() {
+        engine.processorIsWork();
     }
 };
 
-class Phone : protected Mobile {
+class GraphicsCard {
 private:
-    string simCount;
+    Computer engine;
 public:
-    Phone(string a, string b, string c) : Mobile(a, b) {
-        simCount = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nBattery Size: " << batterySize << "\nSim Count: " << simCount;
+    void printer() {
+        engine.graphicsCardIsWork();
     }
 };
 
-class SmartWatch : protected Mobile {
+class Motherboard {
 private:
-    string size;
+    Computer engine;
 public:
-    SmartWatch(string a, string b, string c) : Mobile(a, b) {
-        size = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nBattery Size: " << batterySize << "\nSize: " << size;
+    void printer() {
+        engine.motherboardIsWork();
     }
 };
 
-class Laptop : protected Mobile {
+class RAM {
 private:
-    string ram;
+    Computer engine;
 public:
-    Laptop(string a, string b, string c) : Mobile(a, b) {
-        ram = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nBattery Size: " << batterySize << "\nRam: " << ram;
+    void printer() {
+        engine.ramIsWork();
     }
 };
 
-class Computer : protected Stationary {
+class HardDrive {
 private:
-    string cpuCoresAmount;
+    Computer engine;
 public:
-    Computer(string a, string b, string c) : Stationary(a, b) {
-        cpuCoresAmount = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nDiagonal: " << diagonal << "\n cpuCoresAmount: " << cpuCoresAmount;
+    void printer() {
+        engine.hardDriveIsWork();
     }
 };
 
-class TV : protected Stationary {
-private:
-    string model;
-public:
-    TV(string a, string b, string c) : Stationary(a, b) {
-        model = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nDiagonal: " << diagonal << "\n Model: " << model;
-    }
-};
-
-class Monitor : protected Stationary {
-private:
-    string refreshRate;
-public:
-    Monitor(string a, string b, string c) : Stationary(a, b) {
-        refreshRate = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nDiagonal: " << diagonal << "\n RefreshRate: " << refreshRate;
-    }
-};
-
-class Playstation : protected Stationary {
-private:
-    string generation;
-public:
-    Playstation(string a, string b, string c) : Stationary(a, b) {
-        generation = c;
-    }
-    void Printer() {
-        cout << "Brand: " << brand << "\nDiagonal: " << diagonal << "\n Generation: " << generation;
-    }
-};
-
-
-
-int main()
-{
-    Tablet tabletObj = Tablet("Xiaomi", "3000", "20");
-    tabletObj.Printer();
-    cout << endl;
+void main() {
+    Processor processorObj;
+    processorObj.printer();
     cout << endl;
 
-    Phone phoneObj = Phone("Samsung", "4000", "2");
-    phoneObj.Printer();
-    cout << endl;
-    cout << endl;
-
-    SmartWatch watchObj = SmartWatch("Apple", "500", "42mm");
-    watchObj.Printer();
-    cout << endl;
+    GraphicsCard graphicsCardObj;
+    graphicsCardObj.printer();
     cout << endl;
 
-    Laptop laptopObj = Laptop("Dell", "6000", "16GB");
-    laptopObj.Printer();
-    cout << endl;
-    cout << endl;
-
-    Computer computerObj = Computer("HP", "27", "8");
-    computerObj.Printer();
-    cout << endl;
+    Motherboard motherboardObj;
+    motherboardObj.printer();
     cout << endl;
 
-    TV tvObj = TV("LG", "55", "OLED");
-    tvObj.Printer();
-    cout << endl;
-    cout << endl;
-
-    Monitor monitorObj = Monitor("ASUS", "32", "144Hz");
-    monitorObj.Printer();
-    cout << endl;
+    RAM ramObj;
+    ramObj.printer();
     cout << endl;
 
-    Playstation psObj = Playstation("Sony", "50", "PS5");
-    psObj.Printer();
-    cout << endl;
+    HardDrive hardDriveObj;
+    hardDriveObj.printer();
 }
